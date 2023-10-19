409667ac724d0d211a5e6e3e1b0b3bd9f1db189c
Jay-Navarrete <145713825+Jay-Navarrete@users.noreply.github.com>
2023-10-19 04:42:52 -0700

    Add files via upload

--- Navarrete,Mangacop_Hands_on_Exam.py
+++ Navarrete,Mangacop_Hands_on_Exam.py
@@ -1,13 +1,24 @@
-decimal_number = int(input("Enter a Decimal Number: "))
-choice = int(input("Choose a conversion:\n1. Binary\n2. Octal\n3. Hexadecimal\nEnter your choice: "))
+decimal_number = int(input("Enter a decimal number: "))
+choice = int(input("Choose conversion:\n1. Binary\n2. Octal\n3. Hexadecimal\nEnter your choice: "))
 if choice == 1:
-    binary_number = bin(decimal_number)
-    print("Binary Representation: ", binary_number)
+    binary_number = ""
+    while decimal_number > 0:
+        binary_number = str(decimal_number % 2) + binary_number
+        decimal_number = decimal_number // 2
+        print("Binary representation:", binary_number)
 elif choice == 2:
-    octal_number = oct(decimal_number)
-    print("Octal Representation: ", octal_number)
+    octal_number = ""
+    while decimal_number > 0:
+        octal_number = str(decimal_number % 8) + octal_number
+        decimal_number = decimal_number // 8
+        print("Octal representation:", octal_number)
 elif choice == 3:
-    hex_number = hex(decimal_number)
-    print("Hexadecimal Representation: ", hex_number)
+    hex_digits = "0123456789ABCDEF"
+    hex_number = ""
+    while decimal_number > 0:
+        remainder = decimal_number % 16
+        hex_number = hex_digits[remainder] + hex_number
+        decimal_number = decimal_number // 16
+        print("Hexadecimal representation:", hex_number)
 else:
-    print("Invalid Input, Please try again.")
+    print("Invalid choice. Please try again")
